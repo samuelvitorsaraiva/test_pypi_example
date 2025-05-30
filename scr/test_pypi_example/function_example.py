3a48a3e4dc9a2a2bad1f1bb10714be579fce3ab5
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2025-05-30 13:38:20 +0200

    comments

--- scr/test_pypi_example/function_example.py
+++ scr/test_pypi_example/function_example.py
@@ -1,2 +1,6 @@
 def square(x: float | int) -> float:
+    """
+    Return the square of a number
+    :return: float
+    """
     return x * x
--- tests/test_func.py
+++ tests/test_func.py
@@ -2,6 +2,5 @@ from test_pypi_example import square
 
 
 def test_function():
-
     for i in [1, 2, 3, 2, 10, 123, 1.4, 3.2, 0]:
         assert square(i) == i ** 2
